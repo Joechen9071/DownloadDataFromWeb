1d4b4bd238813303494b767e1340028a1ff08ee5
Joechen9071 <nmb9071@gmail.com>
2017-11-12 11:12:55 -0600

    Initial commit

---
+++ DownloadDataFromWeb/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace DownloadDataFromWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient myweb = new WebClient();
+            string url = "file:///C:/Users/Joe/Desktop/FireBase/FireBaseTest.html";
+            byte[] datas = myweb.DownloadData(url);
+            string download = Encoding.ASCII.GetString(datas);
+            string Content = myweb.DownloadString(url);
+            richTextBox1.Text = Content;
+            MessageBox.Show("ALL gud");
+        }
+    }
+}
